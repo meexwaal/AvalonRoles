f648df3051548e48767237e25766c67379baeef5
max <maximusjohnsonus@gmail.com>
2016-12-23 11:48:15 -0700

    Added get names

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var main = function() {
     // Game state data
     var num_players;
+    var names = [];
 
     // Functions
     var get_number;
@@ -28,7 +29,7 @@ var main = function() {
          *     $('input[name=a]').focus().select();
          * });*/
     };
-    $('a#calculate').bind('click', get_number);
+    $('a#num_players_button').bind('click', get_number);
 
     /* $('input[type=text]').bind('keydown', function(e) {
      *     if (e.keyCode == 13) {
@@ -42,10 +43,26 @@ var main = function() {
         $('.num_players_screen').animate({
             left: "-300px"
         }, 200);
+
+        // Populate the page with text boxes for player names
+        for(var i = num_players - 1; i >= 0; i--){
+            $('.get_names_screen').prepend(
+                '<input type="text" name="name' + String(i) + '"> <br>'
+            );
+        }
+
+        $('.get_names_screen').animate({
+            left: "0px"
+        }, 200);
     };
     get_names = function() {
-
+        for(var i = 0; i < num_players; i++){
+            names.push($('input[name="name' + String(i) + '"]').val());
+        }
+        show_get_role_set();
     };
+    $('a#get_players_button').bind('click', get_names);
+
     show_get_role_set = function() {
 
     };
